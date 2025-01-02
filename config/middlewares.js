6ef9f6323fa2e46e35bd97b4d0c0da5de69c5b09
Saptarshi <saptarshimondal768@gmail.com>
2025-01-02 19:15:40 +0530

    Image not visible issue resolved

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,14 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   {
-    name:'strapi::security',
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'",'https:'],
-          'img-src': ["'self'",'data:','blob:','dl.airtable.com'],
-          'media-src': ["'self'",'data:','blob:','dl.airtable.com'],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
